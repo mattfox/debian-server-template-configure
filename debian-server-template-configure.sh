6e8630e5f0e39da0f9c2808ca1f12bb4908fe9be
Matt Fox <matt@tansen.ca>
2018-01-04 22:04:19 -0800

    Support ubuntu

--- debian-server-template-configure.sh
+++ debian-server-template-configure.sh
@@ -4,21 +4,24 @@ if [ "${NEW_IP}" == "" ]; then
   echo "export NEW_IP=192.168.56.x"
   exit 1
 fi
-if [ "$NEW_HOSTNAME" == "" ]; then
+if [ "${NEW_HOSTNAME}" == "" ]; then
   echo "Error: new hostname not provided. Try:"
   echo "export NEW_HOSTNAME=new-hostname"
   exit 1
 fi
-
-HOST_ONLY_IFACE="eth1"
-HOST_ONLY_IFACE_FILE="/etc/network/interfaces.d/${HOST_ONLY_IFACE}"
-ORIGINAL_HOSTNAME="debian-server-template"
+if [ "${HOST_ONLY_IFACE}" == "" ]; then
+  echo "Error: host only interface name not provided. Try:"
+  echo "export HOST_ONLY_IFACE=eth1"
+  exit 1
+fi
 
 # Set IP address on host-only adapter
+HOST_ONLY_IFACE_FILE="/etc/network/interfaces.d/${HOST_ONLY_IFACE}"
 sed --in-place "s/HOST_ONLY_ADDR/${NEW_IP}/" ${HOST_ONLY_IFACE_FILE}
 sed --in-place "s/#//" ${HOST_ONLY_IFACE_FILE} # Remove comments
 
 # Set hostname
+ORIGINAL_HOSTNAME=$(cat /etc/hostname)
 echo ${NEW_HOSTNAME} > /etc/hostname
 sed --in-place "s/${ORIGINAL_HOSTNAME}/${NEW_HOSTNAME}/" /etc/hosts
 
