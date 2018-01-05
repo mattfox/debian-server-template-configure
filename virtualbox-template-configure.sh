9d47135bffd9019a90999fc996d49bd05585340a
Matt Fox <matt@tansen.ca>
2018-01-04 22:51:00 -0800

    Update and rename server-template-configure.sh to virtualbox-template-configure.sh

--- virtualbox-template-configure.sh
+++ virtualbox-template-configure.sh
@@ -16,11 +16,10 @@ if [ "${HOST_ONLY_IFACE}" == "" ]; then
 fi
 
 # Set interface and IP address on host-only adapter
-# In Ubuntu Server, install package ifupdown2 to get interfaces.d.
 HOST_ONLY_IFACE_FILE="/etc/network/interfaces.d/hostonly"
 sed --in-place "s/INTERFACE/${HOST_ONLY_IFACE}/" ${HOST_ONLY_IFACE_FILE}
 sed --in-place "s/HOST_ONLY_ADDR/${NEW_IP}/" ${HOST_ONLY_IFACE_FILE}
-sed --in-place "s/#//" ${HOST_ONLY_IFACE_FILE} # Remove comments
+sed --in-place "s/#//" ${HOST_ONLY_IFACE_FILE}  # Remove comments
 
 # Set hostname
 ORIGINAL_HOSTNAME=$(cat /etc/hostname)
