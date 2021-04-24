e7621031f79caffe50776be34699d803715f4b02
Matt Fox <matt@tansen.ca>
2021-04-24 20:43:07 +0000

    :

--- virtualbox-template-configure.sh
+++ virtualbox-template-configure.sh
@@ -12,11 +12,17 @@ fi
 if [ "${HOST_ONLY_IFACE}" == "" ]; then
   echo "Error: host only interface name not provided. Try:"
   echo "export HOST_ONLY_IFACE=eth1"
+  echo "or"
+  echo "export HOST_ONLY_IFACE=enp0s8"
   exit 1
 fi
 
 # Set interface and IP address on host-only adapter
-HOST_ONLY_IFACE_FILE="/etc/network/interfaces.d/hostonly"
+if [ -d "/etc/netplan" ]; then
+  HOST_ONLY_IFACE_FILE="/etc/netplan/10-enp0s8.yaml"
+else
+  HOST_ONLY_IFACE_FILE="/etc/network/interfaces.d/hostonly"
+fi
 sed --in-place "s/INTERFACE/${HOST_ONLY_IFACE}/" ${HOST_ONLY_IFACE_FILE}
 sed --in-place "s/HOST_ONLY_ADDR/${NEW_IP}/" ${HOST_ONLY_IFACE_FILE}
 sed --in-place "s/#//" ${HOST_ONLY_IFACE_FILE}  # Remove comments
